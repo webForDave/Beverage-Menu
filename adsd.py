30be08d52ebac10b1126583da997d8114ac7b6c6
David Akinola <dakinola54@gmail.com>
2025-04-14 16:29:09 +0100

    properly commented code

--- adsd.py
+++ adsd.py
@@ -1,78 +1,99 @@
+#########################################################
+# Beverage Menu
+# Models a real world ordering system - for beverages.
+# Users start the program and are prompted for an order, 
+# ...reprompted if orders are invalid, 
+# ...or cancelled if a service isn't required any longer
+#########################################################
+
+
+# list of beverages, with their numbers on the list and price.
 menu = {
-    1: ["Tea", 6], 
-    2: ["Coffee", 12], 
-    3: ["Shakes", 9], 
-    4: ["Hot Chocolate", 10]
+    1: ["Tea", 3], 
+    2: ["Coffee", 3], 
+    3: ["Shakes", 5], 
+    4: ["Hot Chocolate", 7],
+    5: ["Coca-Cola", 2],
 }
 
 
+#########################################################################################################
+# MAIN FUNCTION: program entry point
+# calls every other functions in program - for now
 def main():
-    sum = 0
-    while True:
+    sum = 0 # initializes a aariable to hold the total price for a user's order
+    while True: # validity stuff
         try:
-            user_item = get_user_item()
+            user_item = get_user_item() # invokdes the get_user_item functions and stores it in a variable for later usage
 
-            if type(user_item) != int:
-                return user_item
-            else:
-                quantity = get_quantity()
-                sum += user_item * quantity
+            if type(user_item) != int: # checks if user item is not of type integer 
+                return user_item # returns the string, in this case, "order cancelled"
+            else: # some fun stuff to be done if user_input is an integer
+                quantity = get_quantity() # invokes and stores the get_quantity function in a variable
+                if type(quantity == None): # if user breaks with the keyboard or nothing is returned from get_quantity
+                    return "No quantity was specified"
+                else:
+                    sum += user_item * quantity # assigns the sum variable a new value - user item multiplied by the quantity 
             
-            question = input('Anything else? Y/N ').strip().title()
+            question = input('Anything else? Y/N ').strip().title() # prompts the user for something else
 
-            if question == 'N':
+            if question == 'N': # returns the sum value if user is satisfied with order
                 return f"Total price: ${sum}"
-            elif question == 'Y' or question != 'Y':
+            elif question == 'Y' or question != 'Y': # reprompts the user for an additional oders if not satisfied
                 continue
         except ValueError:
             continue
 
-
+##########################################################################################################
+# function: Gets an order from the user
+# prints the list of menu in a sequential order 
+# prompts the user for an order
+# validates order
 def get_user_item():
-    # print list of menu.
-    # prompt user for an item in menu.
-    # reprompt for invalid items.
-    while True:
+    while True: # user order validity check
         try:
-            # .
-            print("X- Cancel Order")
+            # .Prints the list of menu in a sequential order
+            print("X- Cancel Order") # allows the user to cancel an order
             for index, value in menu.items(): # menu items is defined above 
-                print(f"{index} - {value[0]} = ${value[1]}")
+                print(f"\t {index} - {value[0]} = ${value[1]}") # prints the number of an item, it's name and it's price
 
-            # .
-            user_prompt = input("What is your order? ").strip().title()
+            # .Prompts the user for an item
+            user_prompt = input("What is your order? ").strip().title() # converts the input into uppercase and trim whitespace(s)
 
-            # .
-            if user_prompt == "X":
-                return "Order cancelled"
+            # .Actual vality check
+            if user_prompt == "X": # if user enters x or X, based on the print from above
+                return "Order cancelled" # stops the order program
             else:
-                for index, value in menu.items():
-                    if int(user_prompt) == index:
-                        return value[1]
-        # .
+                for index, value in menu.items(): # loops through the keys and values in menu 
+                    if int(user_prompt) == index: # converts user prompt into an integer and checks if it equals the current index
+                        return value[1] # returns the price of the current index
+                    elif int(user_prompt) not in menu.keys(): # checks if the item entered is in the menu number
+                        raise ValueError # raises a value error which is caught below
+        # .Catch errors
         except ValueError:
-            continue
+            return "Not an item in menu" # stops the process if user doesn't enter an appropriate value - a number in the menu printed
         except KeyboardInterrupt:
-            break
+            return "Cancelled with the keyboard" # breaks the program if user interupts with keyboard
 
+
+#########################################################################################################
+# function: Prompts the user for a quantity
+# reprompts if quantity is invalid
 def get_quantity():
-    # prompt user for quantity
-    # reprompt if quantity is not an integer
-    while True:
+    while True: # validity check
         try:
-            quantity = int(input("Quantity? ").strip())
+            quantity = int(input("Quantity? ").strip()) # prompts the user for a quantity and converts it into an integer
 
-            if quantity < 1:
-                continue
+            if quantity < 1: # checks for only positive integer
+                continue # starts the loops all over if user enters zero or a negative integer 
             else:
                 return quantity
-        except ValueError:
+        except ValueError: # starts again if user enters a value not equal to an integer, eg. a sting or character
             continue
-        except KeyboardInterrupt:
-            break
-        except ValueError:
+        except KeyboardInterrupt: # on instances where the user quits the program with the keyboard, breaks out
             break
 
 
+#####################################
 if __name__ == "__main__":
     print(main())
--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-            if user == "X":
-                return "Order cancelled."
-            else:
-                for index, value in menu.items():
-                    if user == index:
-                        user = value[1]
-             
